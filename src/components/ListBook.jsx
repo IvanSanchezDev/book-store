794ae06083c62d954c4fd6da0279d742c92db5b1
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-16 18:21:46 -0500

    feat: :sparkles: Creamos una funcion para agregar los libros seleccionados al store
    usamos dispatch y la funcion addBookToReading para enviar la accion al reducer el cual lo recibe, lo procesa y retorna un nuevo estado

--- src/components/ListBook.jsx
+++ src/components/ListBook.jsx
@@ -1,23 +1,38 @@
+import { addBookToReading } from "../Reducers/listReadingSlice";
+import { useDispatch } from "react-redux";
+
+
+
+
 export const ListBook = ({ listBook }) => {
+
+  const dispatch=useDispatch();
+
+  const addBookHandler=(name)=>{
+    const bookFinded=listBook.find(item=>item.book.title==name)
+    dispatch(addBookToReading(bookFinded));
+  }
+
   return (
     <>
-      <h2>Lista de Libros</h2>
+      <h2>{listBook.length} Libros Disponibles</h2>
       <ul>
         {listBook.map((item) => {
            
           const book = item.book;
           return (
           
-            <li key={item.title}>
+            <li key={book.title}>
               <h2>{book.title}</h2>
               <p>Páginas: {book.pages}</p>
               <p>Género: {book.genre}</p>
-              <img src={book.cover} alt="Portada" />
+              <img src={book.cover} alt="Portada" style={{width:"10%"}} />
               <p>Sinopsis: {book.synopsis}</p>
               <p>Año: {book.year}</p>
               <p>ISBN: {book.ISBN}</p>
               <p>Autor: {book.author.name}</p>
               <p>Otros libros del autor: {book.author.otherBooks.join(", ")}</p>
+              <button onClick={()=>addBookHandler(book.title)}>Agregar a la lista de lectura</button>
             </li>
           );
         })}
