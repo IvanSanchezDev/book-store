aa21e82044244835ea6b898540f00b06977141f0
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-16 19:32:08 -0500

    Diseño Adaptado a requerimientos

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Books } from './page/Books'
+import { Readinglist } from './components/ReadingList';
 
 function App() {
  
 
   return (
     <>
-     <Books/>
+      <div className="container">
+        <div className="row">
+          <div className="col-7"> <Books/></div>
+          <div className="col-5 listReading"><Readinglist/></div>
+        </div>
+      </div>
+    
     </>
   )
 }
--- src/components/ListBook.jsx
+++ src/components/ListBook.jsx
@@ -1,42 +1,31 @@
 import { addBookToReading } from "../Reducers/listReadingSlice";
-import { useDispatch } from "react-redux";
-
-
-
+import { useDispatch, useSelector } from "react-redux";
 
 export const ListBook = ({ listBook }) => {
+  const { books } = useSelector((state) => state.listReadingBooks);
+  const dispatch = useDispatch();
 
-  const dispatch=useDispatch();
-
-  const addBookHandler=(name)=>{
-    const bookFinded=listBook.find(item=>item.book.title==name)
+  const addBookHandler = (name) => {
+    const bookFinded = listBook.find((item) => item.book.title == name);
     dispatch(addBookToReading(bookFinded));
-  }
+  };
 
   return (
     <>
       <h2>{listBook.length} Libros Disponibles</h2>
-      <ul>
-        {listBook.map((item) => {
-           
-          const book = item.book;
-          return (
-          
-            <li key={book.title}>
-              <h2>{book.title}</h2>
-              <p>Páginas: {book.pages}</p>
-              <p>Género: {book.genre}</p>
-              <img src={book.cover} alt="Portada" style={{width:"10%"}} />
-              <p>Sinopsis: {book.synopsis}</p>
-              <p>Año: {book.year}</p>
-              <p>ISBN: {book.ISBN}</p>
-              <p>Autor: {book.author.name}</p>
-              <p>Otros libros del autor: {book.author.otherBooks.join(", ")}</p>
-              <button onClick={()=>addBookHandler(book.title)}>Agregar a la lista de lectura</button>
-            </li>
-          );
-        })}
-      </ul>
+      <h4>{books.length} en la lista de lectura</h4>
+        <div className="books">
+          {listBook.map((item) => {
+            const book = item.book;
+            return (
+              <div key={book.title}>
+                <img src={book.cover} alt="Portada" className="img-fluid"  onClick={() => addBookHandler(book.title)}/>
+              </div>
+            );
+          })}
+        </div>
+     
+     
     </>
   );
 };
